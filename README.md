ce2d6e9a0fc5b8c0ed026ac9625a2dd531bb2a41
GameW1zard <rivenloya@gmail.com>
2023-05-23 21:20:43 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Magic Blog [![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)
+      
+ 
+ ## Table of contents 
+ [Description](#description) 
+ [Installation](#installation) 
+ [Usage](#usage) 
+ [Contribution](#contribution) 
+ [Testing instructions](#testing_instructions) 
+ [Licence](#license) 
+ [Contact information](#contact_information)
+      
+ 
+ ## Description 
+ It is a project written in Javascript for NODE and mySQL using Bootstrap and Handlebars
+       
+ 
+ ## Installation 
+ Its a webpage head to [this link]() and start posting!
+      
+ 
+ ## Usage 
+ Login or register then Have fun posting your thoughts!
+ 
+ ## Contribution 
+ Riven Loya Main work and implmentation
+      
+ 
+ ## Testing_instructions 
+
+ ## License 
+ The Do What the Fuck You Want to Public License
+      
+ 
+ ## Contact_information 
+ [GameW1zard](https://github.com/GameW1zard) 
+ 
+      
+ 
+ Made using W1zards MD Generator
---
+++ server.js
@@ -0,0 +1,33 @@
+const path = require ('path');
+const express = require ('express');
+const sequelize = require ('./config/connection');
+const expressHandlebars = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'secret',
+    cookie: {httpOnly: false, maxAge: 24 * 60 * 60 * 1000, secure: false,},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+console.log(session(sess))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main',hbs }));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on ${PORT}. Visit http://localhost:${PORT} in your browser.`));
+});
